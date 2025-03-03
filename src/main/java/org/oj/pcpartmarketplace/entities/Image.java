990908694dd9cf670b474b34f67289933022823c
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-04 01:08:34 +0200

    Initial setup for Image entity

--- src/main/java/org/oj/pcpartmarketplace/entities/Image.java
+++ src/main/java/org/oj/pcpartmarketplace/entities/Image.java
@@ -1,4 +1,15 @@
 package org.oj.pcpartmarketplace.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "images")
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private byte[] imageData;
 }
