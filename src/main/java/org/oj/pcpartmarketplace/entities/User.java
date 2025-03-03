eea91917da909c3a17f6af2d9ed29399015198ed
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-04 00:46:13 +0200

    Initial user entity setup

--- src/main/java/org/oj/pcpartmarketplace/entities/User.java
+++ src/main/java/org/oj/pcpartmarketplace/entities/User.java
@@ -1,4 +1,29 @@
 package org.oj.pcpartmarketplace.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String profilePicture;
+
+    private Date creationDate;
+
+    private Date lastLogin;
 }
