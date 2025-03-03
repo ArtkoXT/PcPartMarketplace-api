913f8918b3766557f06da13b75a94f1b89b8660a
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-04 00:59:38 +0200

    Initial Category entity setup

--- src/main/java/org/oj/pcpartmarketplace/entities/Category.java
+++ src/main/java/org/oj/pcpartmarketplace/entities/Category.java
@@ -1,4 +1,17 @@
 package org.oj.pcpartmarketplace.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+
+    private String description;
+
 }
