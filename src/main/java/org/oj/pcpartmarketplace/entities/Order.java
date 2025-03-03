93493ec29ee92ccaeea4c7d707edcd7aa6028cb7
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-04 00:55:39 +0200

    Initial order entity setup

--- src/main/java/org/oj/pcpartmarketplace/entities/Order.java
+++ src/main/java/org/oj/pcpartmarketplace/entities/Order.java
@@ -1,4 +1,32 @@
 package org.oj.pcpartmarketplace.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "buyerId")
+    private User buyer;
+
+    @ManyToOne
+    @JoinColumn(name = "sellerId")
+    private User seller;
+
+    @ManyToOne
+    @JoinColumn(name = "productId")
+    private Product product;
+
+    private double totalPrice;
+
+    private String status;
+
+    private Date orderDate;
 }
