9f9837b9a0ae45f21093965c4a7012077f4681c9
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-04 00:59:11 +0200

    Initial Listing entity setup

--- src/main/java/org/oj/pcpartmarketplace/entities/Listing.java
+++ src/main/java/org/oj/pcpartmarketplace/entities/Listing.java
@@ -1,4 +1,28 @@
 package org.oj.pcpartmarketplace.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "listings")
 public class Listing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "productId")
+    private Product product;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "sellerId")
+    private User seller;
+
+    private String status;
+
+    private Date createdAt;
+
+    private Date updatedAt;
 }
