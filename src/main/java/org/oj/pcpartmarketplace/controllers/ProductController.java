11449ec2557fd5564ace6d980ba39e473edfb953
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-06 01:05:42 +0200

    Added an endpoint to get all products to ProductController

--- src/main/java/org/oj/pcpartmarketplace/controllers/ProductController.java
+++ src/main/java/org/oj/pcpartmarketplace/controllers/ProductController.java
@@ -5,12 +5,19 @@ import org.oj.pcpartmarketplace.entities.Product;
 import org.oj.pcpartmarketplace.services.ProductService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/products")
 @AllArgsConstructor
 public class ProductController {
 
     ProductService productService;
+
+    @GetMapping("/all")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
 
     @PostMapping("/add")
     public Product addProduct(@RequestBody Product product){
