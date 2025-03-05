e1afd69a8e10732301830d3b331695085b4e8c03
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-06 01:08:27 +0200

    Added a createCategory function to CategoryService

--- src/main/java/org/oj/pcpartmarketplace/services/CategoryService.java
+++ src/main/java/org/oj/pcpartmarketplace/services/CategoryService.java
@@ -1,4 +1,17 @@
 package org.oj.pcpartmarketplace.services;
 
+import lombok.AllArgsConstructor;
+import org.oj.pcpartmarketplace.entities.Category;
+import org.oj.pcpartmarketplace.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class CategoryService {
+
+    CategoryRepository categoryRepo;
+
+    public Category createCategory(Category category){
+        return categoryRepo.save(category);
+    }
 }
