1f75151cf07cb7175cad222d4aaf034e9ae12606
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-06 01:05:23 +0200

    Added getAllProducts function to ProductService

--- src/main/java/org/oj/pcpartmarketplace/services/ProductService.java
+++ src/main/java/org/oj/pcpartmarketplace/services/ProductService.java
@@ -6,12 +6,18 @@ import org.oj.pcpartmarketplace.entities.Product;
 import org.oj.pcpartmarketplace.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Data
 @AllArgsConstructor
 public class ProductService {
 
     ProductRepository productRepo;
+
+    public List<Product> getAllProducts(){
+        return productRepo.findAll();
+    }
 
     public Product createProduct(Product product){
         return productRepo.save(product);
