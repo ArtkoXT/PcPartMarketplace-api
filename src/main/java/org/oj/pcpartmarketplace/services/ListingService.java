28f90f6de09b8f8b1bd6d4e591436f65888e2ae3
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-05 14:41:53 +0200

    Created a service class for Listings

---
+++ src/main/java/org/oj/pcpartmarketplace/services/ListingService.java
@@ -0,0 +1,52 @@
+package org.oj.pcpartmarketplace.services;
+
+import lombok.Data;
+import org.oj.pcpartmarketplace.entities.DTOs.ListingDto;
+import org.oj.pcpartmarketplace.entities.Listing;
+import org.oj.pcpartmarketplace.exceptions.NotFoundException;
+import org.oj.pcpartmarketplace.repositories.ListingRepository;
+import org.oj.pcpartmarketplace.repositories.ProductRepository;
+import org.oj.pcpartmarketplace.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+public class ListingService {
+
+    ListingRepository listingRepo;
+    ProductRepository productRepo;
+    UserRepository userRepo;
+
+    public ListingDto createListing(ListingDto listingDto){
+        return toListingDto(listingRepo.save(createListingFromDTO(listingDto)));
+    }
+
+    private Listing createListingFromDTO(ListingDto listingDto){
+
+        Listing listing = new Listing();
+        listing.setId(listingDto.getId());
+        listing.setProduct(productRepo.findById(listingDto.getProductId())
+                .orElseThrow( () -> new NotFoundException("Product not found!")));
+        listing.setSeller(userRepo.findById(listingDto.getSellerId())
+                .orElseThrow( () -> new NotFoundException("User not found!")));
+        listing.setStatus(listingDto.getStatus());
+        listing.setCreatedAt(listingDto.getCreatedAt());
+        listing.setUpdatedAt(listingDto.getUpdatedAt());
+
+        return listing;
+    }
+
+    private ListingDto toListingDto(Listing listing){
+
+        ListingDto listingDto = new ListingDto();
+        listingDto.setId(listing.getId());
+        listingDto.setProductId(listing.getProduct().getId());
+        listingDto.setSellerId(listing.getSeller().getId());
+        listingDto.setStatus(listing.getStatus());
+        listingDto.setCreatedAt(listing.getCreatedAt());
+        listingDto.setUpdatedAt(listing.getUpdatedAt());
+
+        return listingDto;
+    }
+
+}
