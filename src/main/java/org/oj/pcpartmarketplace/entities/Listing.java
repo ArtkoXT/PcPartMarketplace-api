e0190788bd17c3d6cb1e7ec82e0014185afc3ad4
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-05 14:39:50 +0200

    Added @Data lombok annotation to Listing entity

--- src/main/java/org/oj/pcpartmarketplace/entities/Listing.java
+++ src/main/java/org/oj/pcpartmarketplace/entities/Listing.java
@@ -1,11 +1,13 @@
 package org.oj.pcpartmarketplace.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "listings")
+@Data
 public class Listing {
 
     @Id
