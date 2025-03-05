2b9a1b5ad959aef76f9c09598bac26d58355cb83
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-05 16:07:36 +0200

    Added product creation endpoint to ProductController

--- src/main/java/org/oj/pcpartmarketplace/controllers/ProductController.java
+++ src/main/java/org/oj/pcpartmarketplace/controllers/ProductController.java
@@ -1,4 +1,22 @@
 package org.oj.pcpartmarketplace.controllers;
 
+import lombok.Data;
+import org.oj.pcpartmarketplace.entities.Product;
+import org.oj.pcpartmarketplace.services.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@Data
 public class ProductController {
+
+    ProductService productService;
+
+    @PostMapping("/add")
+    public Product addProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
 }
