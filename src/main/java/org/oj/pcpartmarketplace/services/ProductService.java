a9d51f3cb43de9186e4d85c19d7ade3400842cb9
ArtkoXT <101877782+ArtkoXT@users.noreply.github.com>
2025-03-05 16:07:58 +0200

    Added a createProduct function to ProductService

--- src/main/java/org/oj/pcpartmarketplace/services/ProductService.java
+++ src/main/java/org/oj/pcpartmarketplace/services/ProductService.java
@@ -1,4 +1,19 @@
 package org.oj.pcpartmarketplace.services;
 
+import lombok.Data;
+import org.oj.pcpartmarketplace.entities.Product;
+import org.oj.pcpartmarketplace.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
 public class ProductService {
+
+    ProductRepository productRepo;
+
+    public Product createProduct(Product product){
+        return productRepo.save(product);
+    }
+
+
 }
